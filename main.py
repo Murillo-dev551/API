7b9440beeac097c21a198a2372325927aa47e1df
Murillo-dev551 <murillooliver87@gmail.com>
2025-11-25 19:11:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def conectar():
     db_user = os.environ.get("root")      # Lê a variável de ambiente USER (não use 'root' fixo!)
     db_password = os.environ.get("MaVnBacdRdIKmwalHxaxQNMsBBaqzOpY") # Lê a variável de ambiente PASSWORD
     db_name = os.environ.get("railway")  # Lê a variável de ambiente DATABASE
-    db_port = os.environ.get("3306")
+    db_port = os.environ.get("39014")
 
     return pymysql.connect(
         host=db_host,
@@ -130,6 +130,7 @@ def buscar_tcc():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 3000))
     app.run(host='0.0.0.0', port=port)
+
 
 
 
